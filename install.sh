5aecba75d8626d8e65c21b5678362b09c25ade82
Barmarko <marco@barrella.it>
2020-03-16 03:42:32 +0100

    Fix to provisioning

--- install.sh
+++ install.sh
@@ -67,10 +67,20 @@ echo ""
 echo ""
 echo ""
 
-docker exec -ti php7_trident /bin/bash -c 'cd /var/www/trident-test-app; mv .env.dist .env';
+docker exec -ti php7_trident /bin/bash -c 'cd /var/www/trident-test-app; mv .env.dist .env; mkdir -p config/jwt;';
 
-docker exec -ti php7_trident /bin/bash -c "cd /var/www/trident-test-app; sed -i -- 's/%APPENV%/$(openssl rand -base64 12)/g' .env; sed -i -- 's/%APPSECRET%/prod/g' .env; sed -i -- 's/%JWT_PASSPHRASE%/$(openssl rand -base64 12)/g' .env;";
+docker exec -ti php7_trident /bin/bash -c 'cd /var/www/trident-test-app; openssl genpkey -out config/jwt/private.pem -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096 -pass pass:$JWTPWD;';
 
-docker exec -ti php7_trident /bin/bash -c "cd /var/www/trident-test-app; sed -i -- 's/%MYSQL_DB_USER%/$MYSQL_DB_USER/g' .env; sed -i -- 's/%MYSQL_DB_PASSWORD%/$MYSQL_DB_PASSWORD/g' .env; sed -i -- 's/%MYSQL_DB_HOST%/$MYSQL_DB_HOST/g' .env; sed -i -- 's/%MYSQL_DB_PORT%/$MYSQL_DB_PORT/g' .env; sed -i -- 's/%MYSQL_DB_NAME%/$MYSQL_DB_NAME/g' .env; sed -i -- 's/%MYSQL_VERSION%/$MYSQL_VERSION/g' .env;";
+docker exec -ti php7_trident /bin/bash -c 'cd /var/www/trident-test-app; openssl pkey -in config/jwt/private.pem -out config/jwt/public.pem -pubout -passin pass:$JWTPWD';
+
+docker exec -ti php7_trident /bin/bash -c 'cd /var/www/trident-test-app; sed -i -- "s/%APPSECRET%/9uijhgfrghvcf/g" .env; sed -i -- "s/%APPENV%/dev/g" .env; sed -i -- "s/%JWT_PASSPHRASE%/$JWTPWD/g" .env;';
+
+docker exec -ti php7_trident /bin/bash -c 'cd /var/www/trident-test-app; sed -i -- "s/%MYSQL_DB_USER%/$MYSQL_DB_USER/g" .env; sed -i -- "s/%MYSQL_DB_PASSWORD%/$MYSQL_DB_PASSWORD/g" .env; sed -i -- "s/%MYSQL_DB_HOST%/$MYSQL_DB_HOST/g" .env; sed -i -- "s/%MYSQL_DB_PORT%/$MYSQL_DB_PORT/g" .env; sed -i -- "s/%MYSQL_DB_NAME%/$MYSQL_DB_NAME/g" .env; sed -i -- "s/%MYSQL_VERSION%/$MYSQL_VERSION/g" .env;';
 
 docker exec -ti php7_trident /bin/bash -c 'cd /var/www/trident-test-app; composer install';
+
+docker exec -ti php7_trident /bin/bash -c 'cd /var/www/trident-test-app; php bin/console doctrine:database:drop --force && php bin/console doctrine:database:create';
+
+docker exec -ti php7_trident /bin/bash -c 'cd /var/www/trident-test-app; php bin/console doctrine:migrations:migrate';
+
+docker exec -ti php7_trident /bin/bash -c 'cd /var/www/trident-test-app; php bin/console doctrine:fixtures:load';
